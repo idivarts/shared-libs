e9f750d1133ac4c76be747b6e96767179f37d0bb
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-22 00:14:44 +0530

    Added axios for api calls

--- utilities/HttpWrapper.ts
+++ utilities/HttpWrapper.ts
@@ -1,5 +1,11 @@
-export default class HttpWrapper {
-    constructor() {
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.BACKEND_URL
+class HttpWrapper {
+    callUrl = () => {
 
     }
 }
+
+const httpWrapper = new HttpWrapper();
+export default httpWrapper
