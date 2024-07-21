bf994f38edeac012c99bf6def680b5641530a978
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-21 20:37:15 +0530

    Added hook setup for all repos

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -6,8 +6,8 @@ chmod +x .git/hooks/pre-commit
 cp ./shared-libs/scripts/pre-commit .git/modules/shared-libs/hooks/pre-commit
 chmod +x .git/modules/shared-libs/hooks/pre-commit
 
-cp ./shared-libs/scripts/pre-commit .git/modules/shared-libs/hooks/pre-commit
-chmod +x .git/modules/shared-libs/hooks/pre-commit
+cp ./shared-libs/scripts/pre-commit .git/modules/shared-uis/hooks/pre-commit
+chmod +x .git/modules/shared-uis/hooks/pre-commit
 
-cp ./shared-libs/scripts/pre-commit .git/modules/shared-libs/hooks/pre-commit
-chmod +x .git/modules/shared-libs/hooks/pre-commit
+cp ./shared-libs/scripts/pre-commit .git/modules/shared-constants/hooks/pre-commit
+chmod +x .git/modules/shared-constants/hooks/pre-commit
