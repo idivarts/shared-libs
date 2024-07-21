c7fc60900d95a641687687698255e7be508864ef
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-21 20:32:24 +0530

    Added type checker in all sub repo

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -6,10 +6,8 @@ chmod +x .git/hooks/pre-commit
 cp ./shared-libs/scripts/pre-commit .git/modules/shared-libs/hooks/pre-commit
 chmod +x .git/modules/shared-libs/hooks/pre-commit
 
-# touch shared-uis/.git/hooks/pre-commit
-# cp ./shared-libs/scripts/pre-commit shared-uis/.git/hooks/pre-commit
-# chmod +x shared-uis/.git/hooks/pre-commit
+cp ./shared-libs/scripts/pre-commit .git/modules/shared-libs/hooks/pre-commit
+chmod +x .git/modules/shared-libs/hooks/pre-commit
 
-# touch shared-constants/.git/hooks/pre-commit
-# cp ./shared-libs/scripts/pre-commit shared-constants/.git/hooks/pre-commit
-# chmod +x shared-constants/.git/hooks/pre-commit
+cp ./shared-libs/scripts/pre-commit .git/modules/shared-libs/hooks/pre-commit
+chmod +x .git/modules/shared-libs/hooks/pre-commit
