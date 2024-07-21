70495258b4cf3b58af778b2543fabd19f9d320fe
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-21 20:30:38 +0530

    Checking typescript

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -2,3 +2,14 @@
 
 cp ./shared-libs/scripts/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+
+cp ./shared-libs/scripts/pre-commit .git/modules/shared-libs/hooks/pre-commit
+chmod +x .git/modules/shared-libs/hooks/pre-commit
+
+# touch shared-uis/.git/hooks/pre-commit
+# cp ./shared-libs/scripts/pre-commit shared-uis/.git/hooks/pre-commit
+# chmod +x shared-uis/.git/hooks/pre-commit
+
+# touch shared-constants/.git/hooks/pre-commit
+# cp ./shared-libs/scripts/pre-commit shared-constants/.git/hooks/pre-commit
+# chmod +x shared-constants/.git/hooks/pre-commit
