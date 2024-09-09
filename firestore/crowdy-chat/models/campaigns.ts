64159584c9da84021af79f5c48c63fd45d418b00
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2024-09-09 21:04:50 +0530

    Added Exports in campaing stages

--- firestore/crowdy-chat/models/campaigns.ts
+++ firestore/crowdy-chat/models/campaigns.ts
@@ -1,58 +1,59 @@
 import { ICollection } from "./collections";
 
-export const getCampaignPath = (orgId: string, campaignId?: string) => `/organizations/${orgId}/campaigns/${campaignId}`
+export const getCampaignPath = (orgId: string, campaignId?: string) =>
+  `/organizations/${orgId}/campaigns/${campaignId}`;
 
 export interface ICampaigns {
-    organizationId: string,
+  organizationId: string;
 
-    name: string;
-    objective: string;
-    createdBy: string
-    createdAt: number
-    updatedAt: number
-    status: number
+  name: string;
+  objective: string;
+  createdBy: string;
+  createdAt: number;
+  updatedAt: number;
+  status: number;
 
-    replySpeed: {
-        min: number;
-        max: number;
-    };
-    reminderTiming: {
-        min: number;
-        max: number;
-    };
-    chatgpt: {
-        prescript: string;
-        purpose: string;
-        actor: string;
-        examples: string;
-    };
-    leadStages: ICollection<LeadStage>;
-}
-
-interface LeadStage {
-    organizationId: string,
-    campaignId: string,
-
-    name: string;
+  replySpeed: {
+    min: number;
+    max: number;
+  };
+  reminderTiming: {
+    min: number;
+    max: number;
+  };
+  chatgpt: {
+    prescript: string;
     purpose: string;
-    collectibles: ICollection<Collectible>;
-    reminders: {
-        state: boolean;
-        reminderCount: number;
-        reminderExamples: string;
-    };
-    exampleConversations: string;
-    stopConversation: boolean;
-    leadConversion: boolean;
+    actor: string;
+    examples: string;
+  };
+  leadStages?: ICollection<ILeadStage>;
 }
 
-interface Collectible {
-    organizationId: string,
-    campaignId: string,
-    leadStageId: string,
+export interface ILeadStage {
+  organizationId: string;
+  campaignId: string;
 
-    name: string;
-    type: string;
-    description: string;
-    mandatory: boolean;
+  name: string;
+  purpose: string;
+  collectibles: ICollection<ICollectible>;
+  reminders: {
+    state: boolean;
+    reminderCount: number;
+    reminderExamples: string;
+  };
+  exampleConversations: string;
+  stopConversation: boolean;
+  leadConversion: boolean;
+}
+
+export interface ICollectible {
+  organizationId: string;
+  campaignId: string;
+  leadStageId: string;
+
+  name: string;
+  type: string;
+  description: string;
+  mandatory: boolean;
 }
