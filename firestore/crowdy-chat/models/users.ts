2e682417934f635362f85c55ab21a427baec678f
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-09-09 23:38:30 +0530

    Added preferences

--- firestore/crowdy-chat/models/users.ts
+++ firestore/crowdy-chat/models/users.ts
@@ -4,4 +4,10 @@ export interface IUser {
     username?: string, // This is only needed if you are creating members
     name?: string,
     image?: string
+    preferences?: {
+        theme?: 'light' | 'dark'
+        question1: string,
+        question2: string,
+        question3: string
+    }
 }
