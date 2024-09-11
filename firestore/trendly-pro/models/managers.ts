992a55fa0c8aa4376e7829aa7fbee2a97f23f3c2
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-09-11 21:22:22 +0530

    Made some changes on the interfaces and also rules

--- firestore/trendly-pro/models/managers.ts
+++ firestore/trendly-pro/models/managers.ts
@@ -4,9 +4,9 @@ import { INotifications } from "./notifications";
 export interface IManagers {
     name: string; // Name of the manager
     email: string; // Email of the manager
-    phoneNumber: string; // Phone number of the manager
-    location: string; // Location of the manager
-    dateOfBirth: string; // Date of birth of the manager
+    phoneNumber?: string; // Phone number of the manager
+    location?: string; // Location of the manager
+    // dateOfBirth?: string; // Date of birth of the manager
 
     notifications: ICollection<INotifications>; // Notifications for the manager
 }
--- firestore/trendly-pro/models/users.ts
+++ firestore/trendly-pro/models/users.ts
@@ -4,19 +4,19 @@ import { INotifications } from "./notifications";
 
 export interface IUsers {
     name: string; // Name of the user
-    email: string; // Email of the user
-    phoneNumber: string; // Phone number of the user
-    location: string; // Location of the user
-    dateOfBirth: string; // Date of birth of the user
+    email?: string; // Email of the user
+    phoneNumber?: string; // Phone number of the user
+    location?: string; // Location of the user
+    // dateOfBirth?: string; // Date of birth of the user
     preferences?: {
-        theme?: 'light' | 'dark'
-        question1: string,
-        question2: string,
-        question3: string
+        question1?: string,
+        question2?: string,
+        question3?: string
     }, // User preferences
     settings?: {
-        emailNotification: boolean,
-        pushNotification: boolean
+        theme?: 'light' | 'dark'
+        emailNotification?: boolean,
+        pushNotification?: boolean
     }, // User settings
 
     notifications: ICollection<INotifications>; // Notifications for the user
@@ -27,10 +27,10 @@ export interface ISocials {
     id: string; // Social media ID
     userId: string; // User ID
     platform: SocialPlatform; // Social media platform
-    handle: string; // Social media handle
-    url: string; // URL of the social media profile
-    followers: number; // Number of followers
-    following: number; // Number of accounts following
-    posts: number; // Number of posts
-    engagementRate: number; // Social media engagement rate
+    handle?: string; // Social media handle
+    url?: string; // URL of the social media profile
+    followers?: number; // Number of followers
+    following?: number; // Number of accounts following
+    // posts: number; // Number of posts
+    // engagementRate: number; // Social media engagement rate
 }
