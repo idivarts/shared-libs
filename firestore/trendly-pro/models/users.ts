ac446ffa28f664d31c9d739364ae4f6bf02667f2
dev-rathore <rathoredvd3001@gmail.com>
2024-10-10 04:49:39 +0530

    schema changes

--- firestore/trendly-pro/models/users.ts
+++ firestore/trendly-pro/models/users.ts
@@ -3,34 +3,40 @@ import { SocialPlatform } from "../constants/social-platform";
 import { INotifications } from "./notifications";
 
 export interface IUsers {
-    name: string; // Name of the user
-    email?: string; // Email of the user
-    phoneNumber?: string; // Phone number of the user
-    location?: string; // Location of the user
-    // dateOfBirth?: string; // Date of birth of the user
-    preferences?: {
-        question1?: string,
-        question2?: string,
-        question3?: string
-    }, // User preferences
-    settings?: {
-        theme?: 'light' | 'dark'
-        emailNotification?: boolean,
-        pushNotification?: boolean
-    }, // User settings
+  name: string; // Name of the user
+  email?: string; // Email of the user
+  phoneNumber?: string; // Phone number of the user
+  location?: string; // Location of the user
+  // dateOfBirth?: string; // Date of birth of the user
+  preferences?: {
+    question1?: string;
+    question2?: string;
+    question3?: string;
+  }; // User preferences
+  profileImage?: string; // Profile image of the user
+  settings?: {
+    theme?: "light" | "dark";
+    emailNotification?: boolean;
+    pushNotification?: boolean;
+  }; // User settings
 
-    notifications: ICollection<INotifications>; // Notifications for the user
-    socials: ICollection<ISocials>; // Social media accounts of the user
+  notifications: ICollection<INotifications>; // Notifications for the user
+  socials: ICollection<ISocials>; // Social media accounts of the user
+  pushNotificationToken: {
+    ios?: string[];
+    android?: string[];
+    web?: string[];
+  };
 }
 
 export interface ISocials {
-    id: string; // Social media ID
-    userId: string; // User ID
-    platform: SocialPlatform; // Social media platform
-    handle?: string; // Social media handle
-    url?: string; // URL of the social media profile
-    followers?: number; // Number of followers
-    following?: number; // Number of accounts following
-    // posts: number; // Number of posts
-    // engagementRate: number; // Social media engagement rate
+  id: string; // Social media ID
+  userId: string; // User ID
+  platform: SocialPlatform; // Social media platform
+  handle?: string; // Social media handle
+  url?: string; // URL of the social media profile
+  followers?: number; // Number of followers
+  following?: number; // Number of accounts following
+  // posts: number; // Number of posts
+  // engagementRate: number; // Social media engagement rate
 }
