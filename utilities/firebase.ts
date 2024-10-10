d15f6ac52a2b70f80f7013f4fb05f01ae3732e5e
Devender Singh Rathore <59788555+dev-rathore@users.noreply.github.com>
2024-10-10 12:19:39 +0530

    made changes for env variables (#16)

--- utilities/firebase.ts
+++ utilities/firebase.ts
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA5gnQrMolevPZLp5ZQkJk6Howz9BWiP60",
-    authDomain: "crowdy-chat.firebaseapp.com",
-    projectId: "crowdy-chat",
-    storageBucket: "crowdy-chat.appspot.com",
-    messagingSenderId: "14145115952",
-    appId: "1:14145115952:web:4d11d64d9ddd9225dbdaf5",
-    measurementId: "G-NCS8Q59G2S"
+  apiKey: process.env.CROWDY_CHAT_API_KEY,
+  authDomain: process.env.CROWDY_CHAT_AUTH_DOMAIN,
+  projectId: process.env.CROWDY_CHAT_PROJECT_ID,
+  storageBucket: process.env.CROWDY_CHAT_STORAGE_BUCKET,
+  messagingSenderId: process.env.CROWDY_CHAT_MESSAGING_SENDER_ID,
+  appId: process.env.CROWDY_CHAT_APP_ID,
+  measurementId: process.env.CROWDY_CHAT_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
