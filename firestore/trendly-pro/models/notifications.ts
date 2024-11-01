124184dc29b0657c9358e3883f48d717c06c72cd
Devender Singh Rathore <59788555+dev-rathore@users.noreply.github.com>
2024-11-01 20:11:35 +0530

    added userId to notification model (#22)

--- firestore/trendly-pro/models/notifications.ts
+++ firestore/trendly-pro/models/notifications.ts
@@ -1,13 +1,13 @@
-
 export interface INotifications {
-    title: string; // Title of the notification
-    description: string; // Description of the notification
-    timeStamp: number; // Posted date and time
-    isRead: boolean; // Indicates if the notification is read
-    data?: {
-        // Mixed
-        collaborationId?: string,
-        groupId?: string,
-    }
-    type: string; // Type of notification (e.g., invite(users), applications(managers), applications-accept(users))
+  title: string; // Title of the notification
+  description: string; // Description of the notification
+  timeStamp: number; // Posted date and time
+  isRead: boolean; // Indicates if the notification is read
+  data?: {
+    // Mixed
+    collaborationId?: string;
+    groupId?: string;
+    userId?: string;
+  };
+  type: string; // Type of notification (e.g., invite(users), applications(managers), application-accepted(users), invitation-accepted(managers))
 }
