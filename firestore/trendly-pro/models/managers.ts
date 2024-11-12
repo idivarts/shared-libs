e834de03dc779ced6d9ae711567f20fdf7c6c232
Devender Singh Rathore <59788555+dev-rathore@users.noreply.github.com>
2024-11-12 11:12:24 +0530

    model changes (#23)

--- firestore/trendly-pro/models/managers.ts
+++ firestore/trendly-pro/models/managers.ts
@@ -2,11 +2,23 @@ import { ICollection } from "../../collections";
 import { INotifications } from "./notifications";
 
 export interface IManagers {
-    name: string; // Name of the manager
-    email: string; // Email of the manager
-    phoneNumber?: string; // Phone number of the manager
-    location?: string; // Location of the manager
-    // dateOfBirth?: string; // Date of birth of the manager
+  name: string; // Name of the manager
+  email: string; // Email of the manager
+  phoneNumber?: string; // Phone number of the manager
+  location?: string; // Location of the manager
+  // dateOfBirth?: string; // Date of birth of the manager
 
-    notifications: ICollection<INotifications>; // Notifications for the manager
+  notifications: ICollection<INotifications>; // Notifications for the manager
+
+  profileImage?: string;
+  settings?: {
+    theme?: "light" | "dark";
+    emailNotification?: boolean;
+    pushNotification?: boolean;
+  };
+  pushNotificationToken: {
+    ios?: string[];
+    android?: string[];
+    web?: string[];
+  };
 }
