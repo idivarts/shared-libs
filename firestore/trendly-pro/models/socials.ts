86668e4906e8dc6f38342821b9c3cab100882b48
Rahul Sinha <rahul@idiv.in>
2024-12-11 22:11:50 +0530

    Added socials json

---
+++ firestore/trendly-pro/models/socials.ts
@@ -0,0 +1,47 @@
+export interface ISocials {
+    id: string;
+    name: string;
+    image: string;
+    isInstagram: boolean;
+    instaProfile?: IInstagramProfile; // Instagram profile will only be present when isInstagram is true
+    fbProfile?: IFacebookProfile; // Facebook profile will only be present when isInstagram is false
+}
+
+export interface IInstagramProfile {
+    name: string;
+    username: string;
+    biography: string;
+    id: string;
+    profilePictureUrl: string;
+    followersCount: number;
+    followsCount: number;
+    mediaCount: number;
+    website: string;
+}
+
+export interface IFacebookProfile {
+    id: string;
+    name: string;
+    about: string;
+    category: string;
+    categoryList: {
+        id: string;
+        name: string;
+    }[];
+    phone: string;
+    location: any; // Use a more specific type if the structure of 'location' is known
+    website: string;
+    emails: any; // Use a more specific type if the structure of 'emails' is known
+    fanCount: number;
+    followersCount: number;
+    picture: {
+        data: {
+            url: string;
+        }
+    };
+    cover: {
+        source: string;
+        id: string;
+    };
+}
+
