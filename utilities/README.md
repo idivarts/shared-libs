04f803fa82956295255f512a63cc4eccef16c9bd
Rahul Sinha <rahul@idiv.in>
2025-01-23 13:17:47 +0530

    Removed firebase from main utils

--- utilities/README.md
+++
@@ -1,1 +0,0 @@
-This folder will contain any utilities to be used in the application
--- utilities/analytics.ts
+++
@@ -1,6 +0,0 @@
-import { getAnalytics } from "firebase/analytics";
-import { FirebaseApp } from "./firebase";
-
-const AnalyticsApp = getAnalytics(FirebaseApp);
-
-export { AnalyticsApp }
