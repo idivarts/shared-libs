9543293d9193e98a5775612850b81bc07ac1583d
Rahul Sinha <rahul@idiv.in>
2025-04-29 23:00:45 +0530

    Moved all the packages outside to shared-lib

---
+++ utils/firebase/firebase.ts
@@ -0,0 +1,10 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+
+const firebaseConfig = require("../../../firebase-config.js").firebaseConfig;
+
+const FirebaseApp = (!getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp())
+
+export { FirebaseApp, firebaseConfig };
+
