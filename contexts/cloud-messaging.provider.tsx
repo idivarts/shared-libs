83627be373db3dbb2e8fd8edaa34bb473dafa616
Rahul Sinha <rahul@idiv.in>
2025-05-03 01:09:37 +0530

    Moved cloud messaging provider outside on shared lib

---
+++ contexts/cloud-messaging.provider.tsx
@@ -0,0 +1,43 @@
+import {
+  createContext,
+  type PropsWithChildren,
+  useContext
+} from "react";
+
+import { useCloudMessaging } from "@/shared-libs/utils/cloud-messaging";
+import { AuthApp } from "@/shared-libs/utils/firebase/auth";
+
+interface CloudMessagingContextProps {
+  getToken: () => Promise<string>;
+  registerPushTokenWithStream: (token: string) => Promise<void>
+  registerPushTokenWithPlatform: (token: string) => Promise<void>
+}
+
+const CloudMessagingContext = createContext<CloudMessagingContextProps>({
+  getToken: async () => "",
+  registerPushTokenWithPlatform: async (x: string) => { },
+  registerPushTokenWithStream: async (x: string) => { },
+});
+
+export const useCloudMessagingContext = () => useContext(CloudMessagingContext);
+
+export const CloudMessagingContextProvider: React.FC<PropsWithChildren & { userOrmanager: any, updateUserOrManager: any, streamClient: any }> = ({
+  children,
+  userOrmanager,
+  updateUserOrManager,
+  streamClient
+}) => {
+  const { getToken, registerPushTokenWithStream, registerPushTokenWithPlatform } = useCloudMessaging(streamClient, AuthApp.currentUser?.uid, userOrmanager, updateUserOrManager)
+
+  return (
+    <CloudMessagingContext.Provider
+      value={{
+        getToken,
+        registerPushTokenWithPlatform,
+        registerPushTokenWithStream
+      }}
+    >
+      {children}
+    </CloudMessagingContext.Provider>
+  );
+};
