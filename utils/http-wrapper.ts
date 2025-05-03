756254bcb79c0740d172bbe5c03dc78e5ad62b14
Rahul Sinha <rahul@idiv.in>
2025-05-03 18:50:20 +0530

    Added idToken

--- utils/http-wrapper.ts
+++ utils/http-wrapper.ts
@@ -2,9 +2,8 @@ import { AuthApp } from "./firebase/auth";
 
 const BASE_URL = 'https://be.trendly.now';
 export class HttpWrapper {
-    public static fetch = async (urlPath: string, init?: RequestInit): Promise<Response> => {
-        let idToken = ""
-        if (AuthApp.currentUser) {
+    public static fetch = async (urlPath: string, init?: RequestInit, idToken = ""): Promise<Response> => {
+        if (AuthApp && AuthApp.currentUser) {
             idToken = await AuthApp.currentUser.getIdToken();
         }
         const response = await fetch(BASE_URL + urlPath, {
