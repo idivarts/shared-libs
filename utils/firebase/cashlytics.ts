77b868d864931faef42376e3e30fcf89a8075f81
Rahul Sinha <rahul@idiv.in>
2025-05-25 13:50:50 +0530

    Added tag as a param

--- utils/firebase/cashlytics.ts
+++ utils/firebase/cashlytics.ts
@@ -3,16 +3,22 @@ import { getCrashlytics } from "@react-native-firebase/crashlytics";
 const crashlytics = getCrashlytics();
 
 export class CrashLog {
-    public static log(message: string): void {
-        console.log(message)
+    public static log(message: string, tag?: string): void {
+        if (tag)
+            console.log(message)
+        else
+            console.log(tag, message);
         crashlytics.log(message);
     }
     public static crash(): void {
         console.log("Crashing the app for testing purposes");
         crashlytics.crash();
     }
-    public static error(error: Error): void {
-        console.error(error);
+    public static error(error: Error, tag?: string): void {
+        if (tag)
+            console.error(error);
+        else
+            console.error(tag, error);
         crashlytics.recordError(error);
     }
 }
