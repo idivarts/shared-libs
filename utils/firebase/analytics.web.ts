c962be70049bd8315f5268b14e684a51910cbff2
Rahul Sinha <rahul@idiv.in>
2025-05-30 23:19:04 +0530

    Minor update

--- utils/firebase/analytics.web.ts
+++ utils/firebase/analytics.web.ts
@@ -1,11 +1,11 @@
-import { Analytics, getAnalytics, logEvent } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 import { FirebaseApp } from "./firebase";
 
-let analyticsWeb: Analytics = getAnalytics(FirebaseApp);;
+const analyticsWeb = getAnalytics(FirebaseApp);;
 
 export const analyticsLogEvent = async (
   eventName: string,
   eventParams: Record<string, any>
 ) => {
-  logEvent(analyticsWeb as Analytics, eventName, eventParams);
+  logEvent(analyticsWeb, eventName, eventParams);
 };
