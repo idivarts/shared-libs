030bdcd58798eda0b98fb43b4ad0b4f530b3457e
Rahul Sinha <rahul@idiv.in>
2025-06-02 18:20:10 +0530

    Added option for different update provider for web

--- contexts/update-provider.tsx
+++ contexts/update-provider.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Alert, Linking, Platform } from 'react-native';
+import { Alert, Linking } from 'react-native';
 import VersionCheck from 'react-native-version-check';
 
 type Props = {
@@ -9,9 +9,6 @@ type Props = {
 
 const UpdateProvider = ({ children, force = false }: Props) => {
     useEffect(() => {
-        if (Platform.OS == "web")
-            return;
-
         const checkVersion = async () => {
             try {
                 const latestVersion = await VersionCheck.getLatestVersion();
---
+++ contexts/update-provider.web.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
+
+type Props = {
+    children: React.ReactNode;
+    force?: boolean;
+};
+
+const UpdateProvider = ({ children, force = false }: Props) => {
+    useEffect(() => {
+        if (Platform.OS != "web")
+            return;
+
+        // Write code to see if a new version available for web build, then empty cache and reload 
+    }, [force]);
+
+    return <>{children}</>;
+};
+
+export default UpdateProvider;
