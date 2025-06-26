2c79e9030250bf6d340ecf951bf45de5ea4e63c4
Rahul Sinha <rahul@idiv.in>
2025-06-26 14:28:26 +0530

    Added id scroll

---
+++ utils/infinite-id-scroll.ts
@@ -0,0 +1,97 @@
+import { CollectionReference, DocumentData, documentId, getDocs, query, Query, where } from "firebase/firestore"
+import { useEffect, useState } from "react"
+import { Console } from "./console"
+
+let lock = false
+
+export const useInfiniteIdScroll = <T>(docIds: string[], queryOrCol: CollectionReference<DocumentData, DocumentData> | Query<DocumentData, DocumentData>,
+    perPage = 5) => {
+
+    const [loading, setLoading] = useState(true)
+    const [nextAvailable, setNextAvailable] = useState(true)
+    const [currentIndex, setCurrentIndex] = useState<number>(0)
+    const [scrollToTop, setScrollToTop] = useState(false)
+
+    const [data, setData] = useState<(T & { documentId: string })[]>([])
+
+    const fetchDocuments = async (data: (T & { documentId: string })[]) => {
+        if (lock) return
+        lock = true
+
+        setLoading(true)
+
+        const qWhere = where(documentId(), "in", docIds.slice(currentIndex, currentIndex + perPage))
+        const docs = await getDocs(query(queryOrCol, qWhere))
+        docs.forEach((doc) => {
+            data.push({
+                ...(doc.data() as T),
+                // name: doc.data().name + " : " + data.length + " - " + doc.id,
+                documentId: doc.id
+            })
+        })
+        setCurrentIndex(currentIndex + perPage)
+        setData([...data])
+        if (currentIndex + perPage > docIds.length) {
+            setNextAvailable(false)
+        }
+        setLoading(false)
+        lock = false
+    }
+
+    useEffect(() => {
+        lock = false
+    }, [])
+
+    useEffect(() => {
+        if (docIds.length == 0)
+            return
+        const q = query(queryOrCol) // Assumes docs have a `createdAt` timestamp
+        setCurrentIndex(0)
+        setNextAvailable(true)
+        setScrollToTop(false)
+        setData([])
+        fetchDocuments([])
+    }, [docIds])
+
+    // useEffect(() => {
+    //     if (nextAvailable) {
+    //         fetchDocuments([])
+    //     }
+    // }, [nextAvailable])
+
+    const loadMore = () => {
+        fetchDocuments(data)
+    }
+    const resetData = () => {
+        // reset all variables
+        setCurrentIndex(0)
+        setNextAvailable(true)
+        setScrollToTop(false)
+        setData([])
+        lock = false;
+        fetchDocuments([])
+    }
+
+    const onScrollEvent = (event: any) => {
+        const { nativeEvent } = event
+        const paddingToBottom = 200;
+        const { layoutMeasurement, contentOffset, contentSize } = nativeEvent;
+        const isNearBottom = layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
+
+        if (isNearBottom && nextAvailable && !loading) {
+            Console.log("Scroll Case", isNearBottom, nextAvailable, !loading, currentIndex);
+            fetchDocuments(data)
+        }
+    }
+
+    return {
+        loading,
+        data,
+        scrollToTop,
+        onScrollEvent,
+        loadMore,
+        resetData,
+        nextAvailable
+    }
+
+}
