7ba34769ade26ed7c995338c2c8482f2da954f48
Rahul Sinha <rahul@idiv.in>
2025-07-08 12:30:18 +0530

    Minor patch update on quotation

--- firestore/trendly-pro/models/collaborations.ts
+++ firestore/trendly-pro/models/collaborations.ts
@@ -65,7 +65,7 @@ export interface IApplications {
   status: string; // "active", "rejected", "accepted"
   timeStamp: number;
   message: string;
-  quotation: number | string;
+  quotation: number;
   answersFromInfluencer: {
     question: number;
     answer: string;
