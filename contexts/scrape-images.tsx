4c7bbc28a9ea958238495de86bacfe89cf91e991
Rahul Sinha <rahul@idiv.in>
2025-07-09 19:30:46 +0530

    Corrected logic for exists

--- contexts/scrape-images.tsx
+++ contexts/scrape-images.tsx
@@ -72,11 +72,10 @@ export const ScrapeImagesProvider: React.FC<ScrapeImagesProviderProps> = ({ chil
         Console.log("Fetched images:", uploadabledAsset.length);
         for (let i = 0; i < uploadabledAsset.length; i++) {
             const uAsset = uploadabledAsset[i];
-            for (let i = 0; i < existingImages.length; i++) {
-                const ele = existingImages[i];
-                if (uAsset.id == ele.id)
-                    continue
-            }
+            let exists = existingImages.find(e => e.id == uAsset.id)
+            if (exists)
+                continue
+
             const att = await uploadFileUri(uAsset).catch(e => { })
             if (att && att.imageUrl) {
                 console.log("Uploaded File", uAsset.id);
