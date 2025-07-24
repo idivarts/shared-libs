4a010e761bb8572fc1753ff1b6b8885c0d844693
Rahul Sinha <rahul@idiv.in>
2025-07-25 01:32:49 +0530

    Added function for expiry logic

--- utils/persistent-storage.ts
+++ utils/persistent-storage.ts
@@ -13,5 +13,26 @@ export const PersistentStorage = {
     clear: async (key: string) => {
         if (Platform.OS === "web") localStorage.removeItem(key);
         else await AsyncStorage.removeItem(key);
+    },
+    setItemWithExpiry: async (key: string, value: string, ttlInHours = 2) => {
+        const now = new Date();
+        const expiry = now.getTime() + ttlInHours * 60 * 60 * 1000;
+        const item = JSON.stringify({ value, expiry });
+        await AsyncStorage.setItem(key, item);
+    },
+    getItemWithExpiry: async (key: string): Promise<string | null> => {
+        const item = await AsyncStorage.getItem(key);
+        if (!item) return null;
+
+        try {
+            const { value, expiry } = JSON.parse(item);
+            if (new Date().getTime() > expiry) {
+                await AsyncStorage.removeItem(key);
+                return null; // expired
+            }
+            return value;
+        } catch {
+            return null;
+        }
     }
 };
