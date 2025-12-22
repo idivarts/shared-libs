e953caced8bbfc2dea99d16a41aca4e2a41470a9
Rahul Sinha <rahul@idiv.in>
2025-12-22 15:37:34 +0530

    Using plain domain

--- utils/http-wrapper.ts
+++ utils/http-wrapper.ts
@@ -1,8 +1,9 @@
 import { AuthApp } from "./firebase/auth";
 
-const HOST_NAME = window.location.hostname;
-const IS_DEV = HOST_NAME.startsWith("localhost") || HOST_NAME.startsWith("dev.")
-const BASE_URL = `https://be.trendly.now${IS_DEV ? "/dev" : ""}`;
+// const HOST_NAME = window.location.hostname;
+// const IS_DEV = HOST_NAME.startsWith("localhost") || HOST_NAME.startsWith("dev.")
+// const BASE_URL = `https://be.trendly.now${IS_DEV ? "/dev" : ""}`;
+const BASE_URL = `https://be.trendly.now`;
 
 export class HttpWrapper {
     public static fetch = async (urlPath: string, init?: RequestInit, idToken = ""): Promise<Response> => {
