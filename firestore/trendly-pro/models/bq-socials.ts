3b300b73dec57b8072b080875e91ec03b3a51ebc
Rahul Sinha <rahul@idiv.in>
2026-02-18 22:42:11 +0530

    Added quality score

--- firestore/trendly-pro/models/bq-socials.ts
+++ firestore/trendly-pro/models/bq-socials.ts
@@ -86,6 +86,7 @@ export interface SocialsBrief {
     profile_verified?: boolean;
     creation_time?: number;
     last_update_time?: number;
+    quality_score?: number; // quality score of the influencer between 0 and 10
 
     isDiscover?: boolean;
 }
